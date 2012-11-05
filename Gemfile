bb790553185a0928652297d966a0d0b1b5d1b841
dbc-apprentice <apprentice@devbootcamp.com>
2012-11-04 17:14:21 -0800

    Twitter Reader in progress

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source :rubygems
 gem 'rjb'
 gem 'twitter'
 gem 'diff-lcs'
+gem 'eventmachine'
--- lib/twitterreader.rb
+++ lib/twitterreader.rb
@@ -5,9 +5,24 @@ class TwitterReader
 	include Credentializer
 	attr_reader :client, :credentials
 
+  MAX_CACHE = 1
+
+  def self.challenger_name(msg)
+    @cache  ||= []
+    unless @cache.size >= MAX_CACHE
+      @client ||= client_with_authentication
+      result = client.search("#{msg}").results.first.attrs
+      id, name = result[:id], result[:user][:name]
+      @cache << [name,id] unless @cache.any? {|n,i| i == id } # That messsage id is already in the cache
+    end
+
+    # FIFO
+    @cache.shift[0]
+  end
+
 	def initialize
 		@credentials = twitter_credentials(self)
-		@client = client_with_authentication
+		@client ||= client_with_authentication
 	end
 
   def get_move(twitter_user_name, unique_key)
@@ -26,3 +41,5 @@ class TwitterReader
     end
     return new_client
 	end #TwitterReader
+
+client.search("#dbc_c4").results.first
